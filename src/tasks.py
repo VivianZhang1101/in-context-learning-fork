2f357170d029dca3b25189a81ff86dcf11becabe
robin luo <luohaozh@gmail.com>
2024-04-01 18:07:29 +0000

    Finished experiement with toy model

--- src/tasks.py
+++ src/tasks.py
@@ -96,8 +96,8 @@ class LinearRegression(Task):
 
     def evaluate(self, xs_b, multi_w = False):
         if multi_w:
-            w_b = torch.randn(self.b_size, xs_b.shape[1], self.n_dims)
-            w_b = w_b.to(xs_b.device)
+            self.multi_w_b = torch.randn(self.b_size, xs_b.shape[1], self.n_dims)
+            w_b = self.multi_w_b.to(xs_b.device)
             w_b = w_b.unsqueeze(-1)
             xs_b = xs_b.unsqueeze(-2)
             ys_b = self.scale * (xs_b @ w_b)[:, :, 0, 0]
